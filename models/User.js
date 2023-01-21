4f715c35ce7784bd5f7806e566ec66a76435769b
lohit244 <lohit244@gmail.com>
2023-01-21 12:46:24 +0530

    force fix git issue

--- models/User.js
+++ models/User.js
@@ -1,25 +1,26 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose"
 import bcrypt from "bcrypt"
 import validator from "validator"
 
 const { isStrongPassword } = validator
 
+// User Schema
 const UserSchema = new Schema({
-	username : {
-		type: String,
-		required: [true, "Username is missing"],
-	}, 
-	password: {
-		type: String,
-		required: [true, "Password is missing"],
-		validate: [isStrongPassword, "Password not strong enough"],
-	}
+  username: {
+    type: String,
+    required: [true, "Username is missing"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is missing"],
+    validate: [isStrongPassword, "Password not strong enough"],
+  }
 })
 
-UserSchema.pre("save", async function (next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+UserSchema.pre("save", async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", UserSchema)
