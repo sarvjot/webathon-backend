7a0ca8f5723672c38ba360b41d48ea83ff65415b
Rituraj Seal <rituraj2001.rs@gmail.com>
2023-01-21 15:41:38 +0530

    change in application route

--- routes/Application.js
+++ routes/Application.js
@@ -2,7 +2,7 @@ const ApplicationController = require('../controllers/application')
 const ApplicationRouter = require('express').Router();
 const authContoller = require('../controllers/authController');
 
-ApplicationRouter.get('/',ApplicationController.getApplication);
+ApplicationRouter.get('/:eventId',ApplicationController.getApplication);
 ApplicationRouter.post('/:eventId',authContoller.validateToken, ApplicationController.postApplication);
 
 module.exports = ApplicationRouter;
