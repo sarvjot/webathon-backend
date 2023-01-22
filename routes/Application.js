83cf971de84d9e0222f7c99838cd75098b5d3cbf
lohit244 <lohit244@gmail.com>
2023-01-22 13:19:41 +0530

    some more stuff

--- routes/Application.js
+++ routes/Application.js
@@ -2,9 +2,16 @@ const ApplicationController = require('../controllers/application')
 const ApplicationRouter = require('express').Router();
 const authContoller = require('../controllers/authController');
 
+// Path: /application/:eventId -> [events]
 ApplicationRouter.get('/:eventId', authContoller.validateToken, ApplicationController.getApplication);
+
+// Path: /application/:eventId <- {message? header: {x-auth-token: token}}
 ApplicationRouter.post('/:eventId', authContoller.validateToken, ApplicationController.postApplication);
+
+// Path: /application/accept/:appliactionId <- {headers:{x-auth-token: token}}
 ApplicationRouter.post('/accept/:applicationId', authContoller.validateToken, ApplicationController.acceptApplication);
+
+// Path: /application/reject/:appliactionId <- {headers:{x-auth-token: token}}
 ApplicationRouter.post('/reject/:applicationId', authContoller.validateToken, ApplicationController.rejectApplication);
 
 module.exports = ApplicationRouter;
