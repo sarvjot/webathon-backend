d74445f58607c42b13c52845e9ff8663ddf94722
Rituraj Seal <rituraj2001.rs@gmail.com>
2023-01-22 18:35:11 +0530

    unaccepted events managed

--- controllers/user.js
+++ controllers/user.js
@@ -1,26 +1,38 @@
-const Event = require('../models/Event');
-const Application = require('../models/Application');
-const { application } = require('express');
+const Event = require('../models/Event')
+const Application = require('../models/Application')
+const { application } = require('express')
 
 const getPartOf = async (req, res) => {
-  const user = req.user;
-  const events = await Event.find({ usersAccepted: user._id });
-  res.send(events);
+  const user = req.user
+  const events = await Event.find({ usersAccepted: user._id })
+  res.send(events)
 }
-const anyTrues = (a)=>{
+const anyTrues = a => {
   const flag = false
-  a.forEach(t=>{
-    if(t) flag = true
+  a.forEach(t => {
+    if (t) flag = true
   })
   return flag
 }
 const appliedEvents = async (req, res) => {
-  const user = req.user;
-  const unacceptedApplications = await Application.find({applicant: user._id});  
-  const allEvents = await Event.find({});
-  const unacceptedEvents = allEvents.filter(e => anyTrues(unacceptedApplications.map(application => application.event === e.id)) );
-  res.send({applications: unacceptedApplications,events: unacceptedEvents});
+  const user = req.user
+  const unacceptedApplications = await Application.find({ applicant: user._id })
+  const allEvents = await Event.find({})
+  const unacceptedEvents = []
+  // console.log(unacceptedApplications)
+  // console.log(allEvents)
+  unacceptedApplications.forEach(e1 =>
+    allEvents.forEach(e2 => {
+      // console.log(e2.id.toString())
+      // console.log(e1.event.toString())
+      if (e2.id.toString() === e1.event.toString()) {
+        console.log(e1.event.toString())
+        unacceptedEvents.push(e2)
+      }
+    })
+  )
+  res.send({ applications: unacceptedApplications, events: unacceptedEvents })
 }
 
-exports.getPartOf = getPartOf;
-exports.appliedEvents = appliedEvents;
+exports.getPartOf = getPartOf
+exports.appliedEvents = appliedEvents
